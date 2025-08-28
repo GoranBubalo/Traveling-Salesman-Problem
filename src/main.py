083db7c8f72b8b8d0629169061f54f9167b167f2
Goran <goranbubalo2422003@gmail.com>
2025-08-28 21:36:02 +0200

    Completed the main file for the genetic algorithm

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import random
 from tsp_problem import generate_cities
+from tsp_problem import calculate_total_distance
 
 NUM_CITIES = 30
 POPULATION_SIZE = 100
@@ -15,8 +16,35 @@ def main():
         route = list(range(NUM_CITIES))
         random.shuffle(route)
         population.append(route)
-    
-                
+
+    best_overall_route = None
+    best_overall_distance = float('inf')
+
+    for generation in range(NUM_GENERATIONS):
+
+        best_in_generation = None
+        best_distance_in_generation = float('inf')    
+
+        for individual in population:
+            distance = calculate_total_distance(individual, cities)
+            if distance < best_distance_in_generation:
+                best_distance_in_generation = distance
+                best_in_generation = individual
+
+        if distance < best_overall_distance:
+            best_overall_distance = best_distance_in_generation
+            best_overall_route = best_in_generation
+        
+        # TODO: Add genetic algorithm operations (selection, crossover, mutation) here
+
+        print(f"Generation {generation + 1} / {NUM_GENERATIONS}, Shortest distance = {best_distance_in_generation:.2f}")
+
+
+        # printing finall results
+        print("\n---End of evolution---")
+        print(f"Shortest distance found: {best_overall_distance:.2f}")
+        print(f"Best route: {best_overall_route}")
+
 
 if __name__ == '__main__':
     main()
