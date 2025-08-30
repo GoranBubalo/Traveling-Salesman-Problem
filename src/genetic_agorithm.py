1a33bccb0bc89c95178db712799cb031eaaa6564
Goran Bubalo <goranbubalo2422003@gmail.com>
2025-08-30 16:17:40 +0200

    Adding pmx logic

--- src/genetic_agorithm.py
+++ src/genetic_agorithm.py
@@ -16,3 +16,33 @@ def tournament_selction(population,cities, k = 3):
             best_individual = individual
 
     return best_individual
+
+def pmx_crossover(parent1, parent2):
+    size = len(parent1)
+    child1, child2 = [-1] * size, [-1] * size
+
+    start_pos = random.randint(0, size - 1)
+    end_pos = random.randint(0, size - 1)
+
+    if start_pos > end_pos:
+        start_pos, end_pos = end_pos, start_pos
+    
+    child1[start_pos:end_pos] = parent1[start_pos:end_pos]
+    child2[start_pos:end_pos] = parent2[start_pos:end_pos]
+
+    def fill_gaps(child, parent1, parent2, start, end):
+        mapping = {}
+        for i in range(start, end):
+            mapping[parent2[i]] = parent1[i]
+        
+        for i in range(size):
+            if child[i] == -1:
+                current_value = parent2[i]
+                while current_value in mapping:
+                    current_value = mapping[current_value]
+                child[i] = current_value
+        
+        fill_gaps(child1, parent1, parent2, start_pos, end_pos)
+        fill_gaps(child2, parent2, parent1, start_pos, end_pos)
+
+        return child1, child2
